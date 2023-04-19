7ab3b4a81a4229e88880eb5b1325dd124bdcbcb0
Ecem Gokdogan <ecemgo@hotmail.com>
2023-04-19 23:27:31 +0300

    uploaded navbar & hamburger menu samples

---
+++ navbar/navbar-v1/main.js
@@ -0,0 +1,6 @@
+let mainNav = document.getElementById("js-menu");
+let navBarToggle = document.getElementById("js-navbar-toggle");
+
+navBarToggle.addEventListener("click", function () {
+  mainNav.classList.toggle("active");
+});
---
+++ navbar/navbar-v2/app.js
@@ -0,0 +1,14 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-link").forEach((link) =>
+  link.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
