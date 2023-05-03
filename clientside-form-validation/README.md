9b5c8568b32a6c788bb3960d9cf2635ddba72470
Ecem Gokdogan <ecemgo@hotmail.com>
2023-05-03 13:52:17 +0300

    added animation & uploaded readme

--- clientside-form-validation/README.md
+++ clientside-form-validation/README.md
@@ -1,3 +1,26 @@
-# Project Starter
+## <img src="https://user-images.githubusercontent.com/13468728/233831804-0f5c7ee5-d654-4c13-9c77-a5bd6dc4fe74.jpg" title="great tricks" alt="great tricks" width="50" height="50"/> Client-side Form Validation
 
-This repository contains HTML, CSS and Javascript files for the beginning of the project.
+### Link
+
+- [Live Site URL](https://clientside-form-validation.netlify.app/)
+- [CodePen](https://codepen.io/ecemgo/pen/vYVWKXG)
+
+### Screenshot
+
+<div align="left">
+<h3 align="center">Form</h3>
+<img src="https://user-images.githubusercontent.com/13468728/235894950-9bd92a69-4be1-4d5c-a700-0f69e59cee86.jpeg" title="clientside form" alt="clientside form" width="800" height="400"/>
+<h3 align="center">Show Message</h3>
+<img src="https://user-images.githubusercontent.com/13468728/235894972-2e0cc462-3732-41d0-817b-f1f04e66e9a8.jpeg" title="clientside form" alt="clientside form" width="800" height="400"/>
+</div>
+
+### Useful Resources
+
+- [Youtube](https://www.youtube.com/watch?v=rsd4FNGTRBw) - The reference is the channel of Florin Pop.
+
+### Contact Me
+
+- Website - [ecemgo.com](https://www.ecemgo.com/)
+- Frontend Mentor - [@ecemgo](https://www.frontendmentor.io/profile/ecemgo)
+- Twitter - [@ecemgo](https://twitter.com/ecemgo)
+- CodePen - [@ecemgo](https://codepen.io/ecemgo)
--- clientside-form-validation/app.js
+++ clientside-form-validation/app.js
@@ -2,18 +2,33 @@ const form = document.getElementById("form");
 const username = document.getElementById("username");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
-const password2 = document.getElementById("password2");
+const confirmpassword = document.getElementById("confirmpassword");
+const header = document.querySelector(".header");
+const button = document.querySelector("button");
+const message = document.querySelector(".message");
 
+//! submit the form when clicking the button
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  checkInputs();
+
+  if (
+    username.value.trim() === "" ||
+    email.value.trim() === "" ||
+    password.value.trim() === "" ||
+    confirmpassword.value.trim() !== password.value.trim()
+  ) {
+    checkInputs();
+  } else {
+    submittedForm();
+  }
 });
 
+//! check whether inputs are empty or not
 function checkInputs() {
   const usernameValue = username.value.trim();
   const emailValue = email.value.trim();
   const passwordValue = password.value.trim();
-  const password2Value = password2.value.trim();
+  const confirmpasswordValue = confirmpassword.value.trim();
 
   if (usernameValue === "") {
     setErrorFor(username, "Username cannot be blank");
@@ -35,15 +50,16 @@ function checkInputs() {
     setSuccessFor(password);
   }
 
-  if (password2Value === "") {
-    setErrorFor(password2, "Password cannot be blank");
-  } else if (passwordValue !== password2Value) {
-    setErrorFor(password2, "Password does not match!");
+  if (confirmpasswordValue === "") {
+    setErrorFor(confirmpassword, "Password cannot be blank");
+  } else if (passwordValue !== confirmpasswordValue) {
+    setErrorFor(confirmpassword, "Password does not match!");
   } else {
-    setSuccessFor(password2);
+    setSuccessFor(confirmpassword);
   }
 }
 
+//! add error class to the form-control
 function setErrorFor(input, message) {
   const formControl = input.parentElement; // .form-control
   const small = formControl.querySelector("small");
@@ -52,13 +68,29 @@ function setErrorFor(input, message) {
   formControl.className = "form-control error";
 }
 
+//! add success class to the form-control
 function setSuccessFor(input) {
   const formControl = input.parentElement;
   formControl.className = "form-control success";
 }
 
+//! check email with Regex
 function isEmail(email) {
   return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
     email
   );
+}
+
+//! show the message of 'submitted'
+function submittedForm() {
+  header.classList.add("hidden");
+  form.classList.add("hidden");
+  message.classList.remove("hidden");
+  ani();
+  setTimeout("location.reload(true);", 3000);
+}
+
+//! add animation to the message
+function ani() {
+  message.classList.add("fade-in");
 }
