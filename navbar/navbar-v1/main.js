4d86a3af3364bdecce02f35a6c746b862e6e3c19
Ecem Gokdogan <ecemgo@hotmail.com>
2023-08-07 14:58:51 +0300

    deleted navbar-v1

--- navbar/navbar-v1/main.js
+++
@@ -1,6 +0,0 @@
-let mainNav = document.getElementById("js-menu");
-let navBarToggle = document.getElementById("js-navbar-toggle");
-
-navBarToggle.addEventListener("click", function () {
-  mainNav.classList.toggle("active");
-});
