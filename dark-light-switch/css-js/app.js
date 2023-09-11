33a7959ac03c3123b65ab17ae306d9ac7e9d1bb0
Ecem Gokdogan <ecemgo@hotmail.com>
2023-09-11 14:04:30 +0300

    deleted dark-light-switch folder

--- dark-light-switch/css-js/app.js
+++
@@ -1,7 +0,0 @@
-const body = document.querySelector("body");
-const toggle = document.getElementById("toggle");
-
-toggle.onclick = function () {
-  toggle.classList.toggle("active");
-  body.classList.toggle("active");
-};
