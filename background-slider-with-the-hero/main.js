a9a161748356fde2d8c0a3da76fb53ccac581116
Ecem Gokdogan <ecemgo@hotmail.com>
2023-10-19 12:10:42 +0300

    uploaded background-slider-with-the-hero

---
+++ background-slider-with-the-hero/main.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper(".swiper", {
+  grabCursor: true,
+  speed: 500,
+  effect: "slide",
+  loop: true,
+  mousewheel: {
+    invert: false,
+    sensitivity: 1,
+  },
+});
+
+swiper.enable();
