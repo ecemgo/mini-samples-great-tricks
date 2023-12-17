44bd4cd74033b77d56b58e3e23b3270809565d76
Ecem Gokdogan <ecemgo@hotmail.com>
2023-12-17 13:50:34 +0200

    uploaded animated slider

---
+++ animated-slider/app.js
@@ -0,0 +1,66 @@
+var swiper = new Swiper(".swiper", {
+  navigation: {
+    nextEl: ".button-next",
+    prevEl: ".button-prev",
+  },
+  effect: "slide",
+  speed: 900,
+  loop: true,
+});
+
+particlesJS("particles-js", {
+  particles: {
+    number: {
+      value: 200,
+      density: {
+        enable: true,
+        value_area: 800,
+      },
+    },
+    color: {
+      value: "#f0c394",
+    },
+    opacity: {
+      value: 0.4,
+      random: false,
+      anim: {
+        enable: false,
+        speed: 1,
+        opacity_min: 0.1,
+        sync: false,
+      },
+    },
+    size: {
+      value: 3,
+      random: true,
+      anim: {
+        enable: false,
+        speed: 40,
+        size_min: 0.1,
+        sync: false,
+      },
+    },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: "#f0c394",
+      opacity: 0.4,
+      width: 1,
+    },
+    move: {
+      enable: true,
+      speed: 0.5,
+      direction: "none",
+      random: false,
+      straight: false,
+      out_mode: "out",
+      bounce: false,
+      attract: {
+        enable: false,
+        rotateX: 600,
+        rotateY: 1200,
+      },
+    },
+  },
+  retina_detect: true,
+});
