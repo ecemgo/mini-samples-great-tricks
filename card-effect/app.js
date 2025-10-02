bc0dd68aa00f9fbb5da59d1ccc47206e37ce849c
Ecem Gokdogan <ecemgo@hotmail.com>
2025-10-02 12:21:14 +0300

    updated the card effect parameter

--- card-effect/app.js
+++ card-effect/app.js
@@ -1,9 +1,11 @@
 var swiper = new Swiper(".swiper", {
   effect: "cards",
+  cardsEffect: {
+    rotate: true,
+  },
   grabCursor: true,
   initialSlide: 2,
   loop: true,
-  rotate: true,
   mousewheel: {
     invert: true,
   },
